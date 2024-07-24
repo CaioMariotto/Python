6b9886c7aa50808208f818a9fb40612247a061e5
CaioMariotto <caio_mariotto@outlook.com>
2024-07-24 12:27:19 -0700

    modified:   ex039.py

--- ex039.py
+++ ex039.py
@@ -5,23 +5,19 @@ de acordo com a idade informe:
 - se ele ainda vai ter que se alistar ao exercito
 - se é a hora de se alistar
 - se ja passou do tempo do alistamento'''
-
+ano_atual=date.today().year
 ano_nasciento=int(input('Digite seu ano de nascimento: '))
-def calcular_idade(ano_nascimento):
-    ano_atual=date.today().year
-    return ano_atual - ano_nasciento
+idade=ano_atual-ano_nasciento
 
-idade=calcular_idade(ano_nasciento)
-
-def verificar_alistamento(idade):
-
-    if idade < 18:
-        print('Voce ainda vai se alistar ao exercito.')
-        
-    elif idade == 18:
-        print('Está na hora de se alistar.')
-    else:
-        print('Já passou do tempo de alistamento')
-
-
-situação=verificar_alistamento(idade)
+if idade == 18:
+        print('Está na hora de se alistar.') 
+elif idade < 18:
+        saldo= 18 - idade
+        ano = saldo+ano_atual 
+        print('Voce ainda precisa se alistar ao exercito.')
+        print('Seu alistamento será em {}'.format(ano))
+else:
+        saldo = idade - 18
+        ano = ano_atual-saldo
+        print('Já passou do tempo de alistamento.')
+        print('Voce deveria ter se alistado em {}'.format(ano))
